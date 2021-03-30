73606763d6426a3b2297afb3b0bb80456f3d6ab1
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-29 21:34:34 -0500

    basic routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 
+// get all users
 router.get('/', (req,res) => {
     res.json({ user: 'yes' });
 });
+// get one user
+router.get('/:id', (req,res) => {
+    res.json({ user: req.params.id });
+});
+// create user
+router.post('/', (req,res) => {
+    res.json({ user: 'created' });
+});
+// update user
+router.put('/:id', (req,res) => {
+    res.json({ user: 'updated' });
+});
+// delete user
+router.delete('/:id', (req,res) => {
+    res.json({ user: 'destroy' });
+})
 
 module.exports = router;
