cb4527d355dcdc4930b2d21725977664ad07c4f4
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-29 22:21:08 -0500

    added api post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,25 +1,59 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
+const { User, Post } = require('../../models');
 
 // get all posts
 router.get('/', (req,res) => {
-    res.json({ user: 'yes' });
+    Post.findAll().then(data => res.json(data)).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 // get one post
 router.get('/:id', (req,res) => {
-    res.json({ user: req.params.id });
+    Post.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data)).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
 });
 // create post
 router.post('/', (req,res) => {
-    res.json({ user: 'created' });
+    Post.create({
+        artist: req.body.artist,
+        album_title: req.body.album_title,
+        genre: req.body.genre,
+        photo_url: req.body.photo_url,
+        user_id: req.body.user_id
+    }).then(data => res.json(data)).catch(err => {
+        console.log(err);
+        res.status(404).json(err);
+    })
 });
 // update post
 router.put('/:id', (req,res) => {
-    res.json({ user: 'updated' });
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data)).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 // delete post
 router.delete('/:id', (req,res) => {
-    res.json({ user: 'destroy' });
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data)).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
 });
 
 module.exports = router;
