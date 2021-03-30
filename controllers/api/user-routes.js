2f6d484c380392192a35446649d61c032bca327d
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-30 10:17:08 -0500

    Added login and logout api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -38,6 +38,47 @@ router.post('/', (req,res) => {
         res.status(400).json(err);
     });
 });
+//login
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+  });
+  //logout
+  router.post('/logout', (req,res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 // update user
 router.put('/:id', (req,res) => {
     User.update(req.body, {
