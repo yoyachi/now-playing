5a1c17940a094faddd8c28ed9ba1301a96bbcccb
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-29 20:55:03 -0500

    First route working

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
+
+router.get('/', (req,res) => {
+    res.json({ post: 'yes' });
+});
+
+module.exports = router;
