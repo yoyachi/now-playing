32787792ede7f27c699574b929249bfe996efd85
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-29 22:09:40 -0500

    user api routes working

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 
+// get all posts
 router.get('/', (req,res) => {
-    res.json({ post: 'yes' });
+    res.json({ user: 'yes' });
+});
+// get one post
+router.get('/:id', (req,res) => {
+    res.json({ user: req.params.id });
+});
+// create post
+router.post('/', (req,res) => {
+    res.json({ user: 'created' });
+});
+// update post
+router.put('/:id', (req,res) => {
+    res.json({ user: 'updated' });
+});
+// delete post
+router.delete('/:id', (req,res) => {
+    res.json({ user: 'destroy' });
 });
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,25 +1,76 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
+const { User, Post } = require('../../models');
 
 // get all users
 router.get('/', (req,res) => {
-    res.json({ user: 'yes' });
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    }).then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 // get one user
 router.get('/:id', (req,res) => {
-    res.json({ user: req.params.id });
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 // create user
 router.post('/', (req,res) => {
-    res.json({ user: 'created' });
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }).then(data => {
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 // update user
 router.put('/:id', (req,res) => {
-    res.json({ user: 'updated' });
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    }).then(data => {
+        if (!data[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(data);
+    })
 });
 // delete user
 router.delete('/:id', (req,res) => {
-    res.json({ user: 'destroy' });
-})
+    User.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(data => {
+          if (!data) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(data);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
 
 module.exports = router;
