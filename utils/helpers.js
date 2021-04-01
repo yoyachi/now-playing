9666522adf81d9a211faf59c9ac0452698edcae0
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-31 23:18:05 -0500

    edit post

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    selectFunction: function(option) {
+        switch(option) {
+            case 'Reggae':
+                return true;
+                break;
+        }
+    }
+}
