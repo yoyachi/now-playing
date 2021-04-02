eb76fd3f19d5ef3c4541e0582bd6716ec286f56e
Raymond Lopez <raymondlopez13@yahoo.com>
2021-04-02 00:13:26 -0500

    added comment model reference to routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { User, Post } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req,res) => {
--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 // homepage
 router.get('/user', (req,res) => {
