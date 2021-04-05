253bad2bb5dad6b61f4cd7c56980601f357edc6f
Cody Kimb <codykimbell@gmail.com>
2021-04-04 23:15:06 -0500

    header img

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static('views/images')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
