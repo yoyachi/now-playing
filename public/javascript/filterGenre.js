a593813ec178d94cd4d2f96562babfe32c689018
Raymond Lopez <raymondlopez13@yahoo.com>
2021-04-08 16:36:37 -0500

    front end js set up

--- public/javascript/filterGenre.js
+++ public/javascript/filterGenre.js
@@ -1,14 +1,27 @@
 async function filterGenre(event) {
     event.preventDefault();
 
-    var genreVal = document.querySelector('#post-genre').value;
+    var genre = document.querySelector('#post-genre').value;
+    var value = document.querySelector(`option[value='${genre}']`).id;
 
-    if (genreVal === "All") {
+    // switch(value) {
+    //     case 'genres':
+    //         document.location.replace(`/${genre}?optgroup=${value}`);
+    //         break;
+    //     case 'decades':
+    //         document.location.replace(`/${genre}?optgroup=${value}`);
+    //         break;
+    //     case 'formats':
+    //         document.location.replace(`/${genre}?optgroup=${value}`);
+    //         break;
+
+    // }
+    if (genre === "All") {
         document.location.replace(`/`);
     }
 
     else {
-        document.location.replace(`/${genreVal}`);
+        document.location.replace(`/${genre}?optgroup=${value}`);
     }
 }
 
